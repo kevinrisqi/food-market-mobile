46d371a93f27f68b9d0f1a7b67bb2d1f3ac081d5
kevin_risqi <kevinrisqi22@gmail.comgit config --global user.email kevinrisqi22@gmail.com>
2021-06-27 18:46:43 +0700

    Create Success Order Page & Success Sign Up Page

--- lib/layout/pages/illustration_page.dart
+++ lib/layout/pages/illustration_page.dart
@@ -21,64 +21,72 @@ class IllustrationPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Container(
-            width: 150,
-            height: 150,
-            margin: EdgeInsets.only(bottom: 50),
-            decoration: BoxDecoration(
-              image: DecorationImage(image: AssetImage(picturePath)),
-            ),
-          ),
-          Text(
-            title,
-            style: blackFontStyle3.copyWith(fontSize: 20),
-          ),
-          Text(
-            subtitle,
-            style: greyFontStyle.copyWith(fontWeight: FontWeight.w300),
-            textAlign: TextAlign.center,
-          ),
-          Container(
-            margin: EdgeInsets.only(top: 30, bottom: 12),
-            width: 200,
-            height: 45,
-            child: ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                primary: mainColor,
-                elevation: 0,
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8)),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: 150,
+                height: 150,
+                margin: EdgeInsets.only(bottom: 50),
+                decoration: BoxDecoration(
+                  image: DecorationImage(image: AssetImage(picturePath)),
+                ),
               ),
-              onPressed: buttonTap1,
-              child: Text(
-                buttonTitle1,
-                style: blackFontStyle3.copyWith(fontWeight: FontWeight.w500),
+              Text(
+                title,
+                style: blackFontStyle3.copyWith(fontSize: 20),
               ),
-            ),
+              Text(
+                subtitle,
+                style: greyFontStyle.copyWith(fontWeight: FontWeight.w300),
+                textAlign: TextAlign.center,
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 30, bottom: 12),
+                width: 200,
+                height: 45,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: mainColor,
+                    elevation: 0,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8)),
+                  ),
+                  onPressed: buttonTap1,
+                  child: Text(
+                    buttonTitle1,
+                    style:
+                        blackFontStyle3.copyWith(fontWeight: FontWeight.w500),
+                  ),
+                ),
+              ),
+              (buttonTap2 == null)
+                  ? SizedBox()
+                  : SizedBox(
+                      width: 200,
+                      height: 45,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          primary: '8D92A3'.toColor(),
+                          elevation: 0,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8)),
+                        ),
+                        onPressed: buttonTap2,
+                        child: Text(
+                          buttonTitle2 ?? title,
+                          style: blackFontStyle3.copyWith(
+                              fontWeight: FontWeight.w500, color: Colors.white),
+                        ),
+                      ),
+                    ),
+            ],
           ),
-          (buttonTap2 == null) ? SizedBox() :
-          SizedBox(
-            width: 200,
-            height: 45,
-            child: ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                primary: '8D92A3'.toColor(),
-                elevation: 0,
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8)),
-              ),
-              onPressed: buttonTap1,
-              child: Text(
-                buttonTitle1,
-                style: blackFontStyle3.copyWith(fontWeight: FontWeight.w500),
-              ),
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,6 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: MainPage()
-    );
+    return GetMaterialApp(debugShowCheckedModeBanner: false, home: MainPage());
   }
 }
